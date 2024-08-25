d82a565d2f61fc80cf828daeb3fb1e30aa5aaae3
Keith Khadar <keithkhadar@gmail.com>
2024-08-25 15:56:00 -0400

    fixed sensor status

--- RobotGUI/src/app/status/status.component.ts
+++ RobotGUI/src/app/status/status.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { RoslibService } from '../roslib-service.service';
+import { BehaviorSubject } from 'rxjs';
 
 export interface SensorElement {
   name: string;
@@ -16,8 +17,9 @@ export interface SensorElement {
   styleUrls: ['./status.component.scss'],
 })
 export class StatusComponent {
-  TopicInfo: {[id: string]: number} = {};
-  dataSource: SensorElement[] = [];
+  Timestamps: {[id: string]: number[]} = {};
+  dataSource = new BehaviorSubject<SensorElement[]>([]);
+
   topics = [
     '/depth',
     '/dvl',
@@ -30,40 +32,52 @@ export class StatusComponent {
   constructor(private roslibService: RoslibService) {}
 
   ngOnInit(): void {
+    // Add the callbacks for when we subscribe to a topic
     this.topics.forEach(topic => {
+      this.Timestamps[topic] = []; // Initialize an empty array
       this.roslibService.subscribeToTopic(topic)
         .subscribe(() => {
-          this.updateRates(topic);
-          this.TopicInfo[topic] = Date.now();
+          // Push the current timestamp into the array
+          this.Timestamps[topic].push(Date.now());
         });
     });
+
+    // Create an interval so that we can update rates
+    const intervalID = setInterval(() => this.updateRates(), 1000);
+
   }
 
-  updateRates(UpdatedTopic: string): void {
-    // Calculate rates based on time intervals
+  updateRates(): void {
     const now = Date.now();
+    let updatedData : SensorElement[] = [];
 
-    // Check if it is not in array already
-    let foundElement = false;
-
-    this.dataSource.forEach(sensorelement => {
-      if(sensorelement.name == UpdatedTopic){
-        const lastUpdate = (this.TopicInfo[UpdatedTopic]) ? this.TopicInfo[UpdatedTopic] : now;
-        const rate = Math.max(0, 1 / ((now - lastUpdate) * 1000)); // Adjust calculation as needed
-        sensorelement.rate = rate;
-        foundElement = true
-        console.log(this.dataSource)
+    for (const id in this.Timestamps){
+      // Make sure the list isnt longer than 10 timestamps
+      if(this.Timestamps[id].length > 10){
+        this.Timestamps[id].shift();
       }
-    });
-    if(!foundElement){
-      this.dataSource.push({name: UpdatedTopic, rate: 0})
+
+      // Get the average rate
+      let avg = 0
+
+      for (let i = 1; i < this.Timestamps[id].length; i++){
+        avg += this.Timestamps[id][i] - this.Timestamps[id][i-1]
+      }
+      if(this.Timestamps[id].length > 1){
+        avg /= (this.Timestamps[id].length -1);
+      }
+
+      // Update the datasource
+      updatedData.push({ name: id, rate: avg == 0 ? 0 : 1000 / avg });
+      this.dataSource.next(updatedData);
     }
   }
+  
 
   getRowClass(row: SensorElement): string {
-    if (row.rate >= 10) {
+    if (row.rate >= 5) {
       return 'high-rate-row';
-    } else if (row.rate >= 5) {
+    } else if (row.rate >= 1) {
       return 'medium-rate-row';
     } else {
       return 'low-rate-row';
